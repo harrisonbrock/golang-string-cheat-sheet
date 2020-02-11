1b156e660fb09b84b19a02517e3462138b5b43b7
Harrison Brock <harrison@harrisonbrock.com>
2020-02-10 21:20:34 -0500

    Added String Test Cases

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
-	
+
+	// Get the String Length.
+	fmt.Println(len("GoLang")) // prints 6
+
+	// Convert String to Title Case.
+	fmt.Println(strings.Title("go is fun to learn.")) // Prints "Go Is Fun To Learn."
+
+	// Check if String Starts with a SubString.
+	fmt.Println(strings.HasPrefix("GoLang", "Go")) // prints true
+
+	// Check if String Ends with SubString.
+	fmt.Println(strings.HasSuffix("GoLang", "ng")) // prints true
+
+	// Compare if Two Strings are Equal (Ignore Case).
+	fmt.Println(strings.EqualFold("GOLANG", "golang")) // prints true
+
 }
